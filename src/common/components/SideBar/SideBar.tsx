68efa224eb52a124fa01d8f076c065a4ff1ef6e1
ysfnmnylmz12 <yusuf.yilmaz@tazebt.com>
2023-03-20 10:42:10 +0300

    added type

--- src/common/components/SideBar/SideBar.tsx
+++ src/common/components/SideBar/SideBar.tsx
@@ -10,11 +10,12 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import './_sidebar.scss';
 import {ProfileSection} from "./Profile";
+import {type IconProp} from "@fortawesome/fontawesome-svg-core";
 
 interface ISelectState {
     selected?: boolean
 }
-const renderSideBarOption = (link: string, icon: any, text: string, { selected }: ISelectState = {}) => {
+const renderSideBarOption = (link: string, icon: IconProp, text: string, { selected }: ISelectState = {}) => {
   return (
     <div className={cx('sidebar__option', { 'sidebar__option--selected': selected })}>
       <FontAwesomeIcon icon={icon} />
