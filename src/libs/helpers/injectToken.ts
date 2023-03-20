696860dabcf4450bec1201b185b82282dd123c35
ysfnmnylmz12 <yusuf.yilmaz@tazebt.com>
2023-03-20 12:17:22 +0300

    fix

--- src/libs/helpers/injectToken.ts
+++ src/libs/helpers/injectToken.ts
@@ -2,6 +2,7 @@ import {AxiosRequestConfig} from "axios";
 import getTokenInfoFromQuery from "./getTokenInfoFromQuery";
 const injectToken = async (config: AxiosRequestConfig) => {
     const a = getTokenInfoFromQuery(document.location.hash)
+    console.log({a})
     if (a.access_token && a.token_type) {
         config.headers.Authorization = `${a.token_type} ${a.access_token}`;
     }
